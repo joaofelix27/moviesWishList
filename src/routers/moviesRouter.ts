28782bac6a23eba4daa66ee38513014d82727c2a
joaofelix27 <joaofelix27@gmail.com>
2022-11-14 15:07:50 -0300

    Setting Repository

---
+++ src/routers/moviesRouter.ts
@@ -0,0 +1,8 @@
+// import { Router } from "express";
+// import { moviesController } from "../controllers/moviesController.js";
+
+// const moviesRouter = Router();
+
+// moviesRouter.post("/", moviesController.insert);
+
+// export default moviesRouter;
